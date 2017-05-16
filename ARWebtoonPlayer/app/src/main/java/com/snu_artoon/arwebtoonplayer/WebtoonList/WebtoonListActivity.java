e257d42b2b9f5777079693a030a303d0efa30f14
Jonghoon Won <baryberri@icloud.com>
2017-05-16 22:28:06 +0900

    Initial commit

---
+++ ARWebtoonPlayer/app/src/main/java/com/snu_artoon/arwebtoonplayer/WebtoonList/WebtoonListActivity.java
@@ -0,0 +1,15 @@
+package com.snu_artoon.arwebtoonplayer.WebtoonList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.snu_artoon.arwebtoonplayer.R;
+
+public class WebtoonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_webtoon_list);
+    }
+}
