503ae8b1e50cdf8762f5bbf565c2a0d83a3d323a
Jonghoon Won <baryberri@icloud.com>
2017-06-05 17:09:59 +0900

    Add AnnotationToXMLConverter.py and finish the implementation of the program

---
+++ AnnotationToXMLConverter/AnnotationToXMLConverter.py
@@ -0,0 +1,36 @@
+import os
+from TextFileReader import *
+
+def main():
+    # get the base directory
+    base_directory = os.getcwd()
+
+    # counter for percentage print
+    stage_counter = 0
+    total_stage = len(os.listdir(base_directory + "/Annotation"))
+
+    for filename in os.listdir(base_directory + "/Annotation"):
+        # print the percentage.
+        stage_counter += 1
+        print(stage_counter, "/", total_stage, end=" ", sep=" ")
+        print("(", int(stage_counter / total_stage * 100), "%)", sep="")
+
+        # check the given file is a valid txt file.
+        if not filename.endswith(".txt"):
+            continue
+
+        # get filename without .txt extension
+        base_filename = truncate_extension(filename, 3)
+
+        # read data from the file
+        image_size = read_image_size(base_directory + "/Image/" + concatenate_extension(base_filename, "jpg"))
+        annotation_data = read_annotation_data(base_directory + "/Annotation/"
+                                               + concatenate_extension(base_filename, "txt"), image_size)
+
+        # write the xml file
+        write_xml_tag(base_directory + "/XML/" + concatenate_extension(base_filename, "xml"),
+                      concatenate_extension(base_filename, "jpg"), image_size, annotation_data)
+
+
+if __name__ == "__main__":
+    main()
--- AnnotationToXMLConverter/TextFileReader.py
+++ AnnotationToXMLConverter/TextFileReader.py
@@ -24,18 +24,20 @@ def read_annotation_data(path: str, image_size):
         lines = file.readlines()
         for line in lines:
             data = line.split(" ")
-            xmin = (data[1] - data[3] / 2) * image_size[0]
-            ymin = (data[2] - data[4] / 2) * image_size[1]
-            xmax = (data[1] + data[3] / 2) * image_size[0]
-            ymax = (data[2] + data[4] / 2) * image_size[1]
+            for i in range(len(data)):
+                data[i] = float(data[i])
+            xmin = int((data[1] - data[3] / 2) * image_size[0])
+            ymin = int((data[2] - data[4] / 2) * image_size[1])
+            xmax = int((data[1] + data[3] / 2) * image_size[0])
+            ymax = int((data[2] + data[4] / 2) * image_size[1])
             bndbox.append((xmin, ymin, xmax, ymax))
 
     return bndbox
 
 
 # write xml tag to the given file
-def write_xml_tag(filename: str, image_size, bndbox):
-    with open(filename, "w") as file:
+def write_xml_tag(filepath: str, filename: str, image_size, bndbox):
+    with open(filepath, "w") as file:
         file.write('<?xml version="1.0"?>')
         file.write('<annotation>')
 
@@ -44,8 +46,8 @@ def write_xml_tag(filename: str, image_size, bndbox):
 
         file.write('<source><database>FDDB</database></source>')
 
-        file.write('<size><width>' + image_size[0] + '</width><height>'
-                   + image_size[1] + '</height><depth>3</depth></size>')
+        file.write('<size><width>' + str(image_size[0]) + '</width><height>'
+                   + str(image_size[1]) + '</height><depth>3</depth></size>')
 
         file.write('<segmented>0</segmented>')
 
@@ -53,10 +55,10 @@ def write_xml_tag(filename: str, image_size, bndbox):
             file.write('<object>')
             file.write('<name>person</name><pose>Unspecified</pose><truncated>0</truncated><difficult>0</difficult>')
             file.write('<bndbox>')
-            file.write('<xmin>' + data[0] + '</xmin>')
-            file.write('<ymin>' + data[1] + '</ymin>')
-            file.write('<xmax>' + data[2] + '</xmax>')
-            file.write('<ymax>' + data[3] + '</ymax>')
+            file.write('<xmin>' + str(data[0]) + '</xmin>')
+            file.write('<ymin>' + str(data[1]) + '</ymin>')
+            file.write('<xmax>' + str(data[2]) + '</xmax>')
+            file.write('<ymax>' + str(data[3]) + '</ymax>')
             file.write('</bndbox></object>')
 
         file.write('</annotation>')
